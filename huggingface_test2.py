d15108db67dd014bd5889c065c478a8a980c8247
Omar Gonzalez <gomarzalez@gmail.com>
2024-04-07 21:38:27 -0700

    Add dependencies for tesseract.js and update npmignore files

--- huggingface_test2.py
+++ huggingface_test2.py
@@ -3,6 +3,7 @@ from PIL import Image
 # testing how i can quickly test a model from huggingface's image to text options
 
 from transformers import AutoModelForImageClassification, PreTrainedTokenizerFast
+import requests
 
 # Load the pre-trained model
 model = AutoModelForImageClassification.from_pretrained("hugginglearners/Ethiopian-Food-Classifier")
@@ -16,21 +17,7 @@ image = open("your_image.jpg", "rb").read()
 # Prepare your image for the model
 inputs = tokenizer(image, return_tensors="pt")
 
-# Make a prediction
-outputs = model(**inputs)
-
-# Print the predicted class
-print(outputs.logits.argmax(-1))
-
-#Possibly send it to a db to retrieve nutritional information - if the model can be prompted do the following:
-
 response = requests.get('https://api.edamam.com/api/food-database/parser?ingr='+food+'&app_id=1b3c5f0b&app_key=5f2b1a7c2a7e7f5e9e0e4c6f5f5b7d2e')
-
-
-model2 = AutoModelForImageClassification.from_pretrained("model-and-image-classification")
-tokenizer = PreTrainedTokenizerFast.from_pretrained("model3")
-
-# Load and process the image
 image = Image.open("user-uploaded-image.jpg")
 processed_image = tokenizer(image, return_tensors="pt")
 
